079aeb75f5f500c8518e29f78e68ae0b8f6b61cd
Yulia <iul.kuzyurina@yandex.ru>
2023-01-19 23:57:44 +0300

    Added Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100 && number > 999)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine($"число в середине - {InCenter(number)}");
+}
+
+return Convert.ToInt32(Console.ReadLine());
+
+int InCenter(int number)
+{
+    int result = ((number / 10) % 10);
+    return result;
+}
+
