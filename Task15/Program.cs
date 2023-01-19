f01d5ab42b520ebc273943360e5a849d2a9c7eb2
Yulia <iul.kuzyurina@yandex.ru>
2023-01-19 22:58:15 +0300

    Added Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число от 1 до 7: ");
+int Daynumber = Convert.ToInt32(Console.ReadLine());
+
+if (Daynumber>0 && Daynumber<8)
+{
+    if (Daynumber == 7 || Daynumber == 6)
+{
+    Console.WriteLine("этот день выходной");
+}
+else Console.WriteLine("не выходной");
+}
