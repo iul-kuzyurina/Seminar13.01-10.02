ba554c0c17e288579f93c3e34e78d2c1f562abf7
Yulia <iul.kuzyurina@yandex.ru>
2023-01-21 00:08:57 +0300

    Added Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+{
+Console.WriteLine("Третьей цифры нет");
+}
+
+int ThirdDigit = Separate(number);
+Console.WriteLine($"Третья цифра - {ThirdDigit}");
+
+int Separate(int number)
+{if (number >= 100)
+
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    return number % 10;
+}
+
+
+
+
+
