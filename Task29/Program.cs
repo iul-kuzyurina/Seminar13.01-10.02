da916e3c1bf97aeba07bed948afc900f869741c7
Yulia <iul.kuzyurina@yandex.ru>
2023-01-26 23:54:35 +0300

    Added Task

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [] nums=new int[8];
+Random r=new Random();
+nums[0]=r.Next(9);
+
+for (int i=1; i<nums.Length;i++)
+{
+    nums[i] = r.Next(9);
+    int a = nums[i];
+}
+
+void PrintArray(int[] arr)
+{    
+    Console.WriteLine(" {0} -> [ {0} ]", string.Join(", ", arr));
+}
+
+PrintArray(nums);
