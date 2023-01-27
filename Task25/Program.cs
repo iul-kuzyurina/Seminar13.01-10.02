a08b6d912a5465e51d6c3b1950a653f6d2dc013b
Yulia <iul.kuzyurina@yandex.ru>
2023-01-27 16:54:54 +0300

    Added Task 25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine(" ");
+//double a =2;
+//double b=3;
+//double c=Math.Pow(a,b);
+//Console.WriteLine($"{c}");
+Console.WriteLine("Введите первое число: ");
+long a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+long b = Convert.ToInt32(Console.ReadLine());
+
+ void naturalNumber(long b)
+{
+    if (b >= 1 )
+    Console.WriteLine($"результат возведения числа a в натуральную степень b = {IntPow(a,b)}");
+    
+    else Console.WriteLine("число не натуральное");
+}
+
+static long IntPow(long a, long b)
+{
+  long result = 1;
+  for (long i = 0; i < b; i++)
+    result *= a;
+  return result;
+}
+naturalNumber(b);
