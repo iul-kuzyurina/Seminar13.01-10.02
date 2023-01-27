0a379872661b39387bdbfb75e3e14d1210345995
Yulia <iul.kuzyurina@yandex.ru>
2023-01-27 17:15:52 +0300

    Added Task 27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int SumDigits(int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        int value = num % 10;
+        sum += value;
+        num /= 10;
+    }
+    return sum;
+}
+
+int summa = SumDigits(number);
+Console.WriteLine("Сумма цифр в числе: " + summa);
