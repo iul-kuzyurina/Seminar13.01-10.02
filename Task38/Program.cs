63900572c5dadefeb312cbdae581f4754e4f63d4
Yulia <iul.kuzyurina@yandex.ru>
2023-01-31 15:10:38 +0300

    Added Task 38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[size];
+CreateArrayRndDouble(array);
+Console.WriteLine("Массив: ");
+PrintArray(array);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int x = 0; x < array.Length; x++)
+{
+    if (array[x] > max)
+        {
+            max = array[x];
+        }
+    if (array[x] < min)
+        {
+            min = array[x];
+        }
+}
+
+Console.WriteLine($"всего {array.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+void CreateArrayRndDouble(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        {
+            array[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] array)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
