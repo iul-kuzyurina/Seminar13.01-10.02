0bd9a806fb00ce756f60e534fb2269fc98c25693
Yulia <iul.kuzyurina@yandex.ru>
2023-02-03 18:24:25 +0300

    Added Task 41

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите элементы(через пробел): ");
+int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+Console.WriteLine("Массив:");
+PrintArray(numbers);
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"количество значений больше 0 = {sum}");
