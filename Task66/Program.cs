3f1327769fdb1ec8133f02989630ad262a2baa94
Yulia <iul.kuzyurina@yandex.ru>
2023-02-15 20:58:54 +0300

    Added Task 66

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,7 +1,7 @@
-﻿//Задайте значения M и number2. Напишите программу, которая найдёт сумму 
-//натуральных элементов в промежутке от M до number2. Выполнить с помощью рекурсии.
-//M = 1; number2 = 15 -> 120
-//M = 4; number2 = 8. -> 30
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму 
+//натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
 
 Console.WriteLine("Введите два числа: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
@@ -9,26 +9,9 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Сумма равна {SumNaturalNumbers(number1, number2)}");
 Console.ReadLine();
 
-//SumNaturalNumbers(number1, number2);
-
-static int SumNaturalNumbers(int num1, int num2)
+int SumNaturalNumbers(int num1, int num2)
 {
-    if (num1 == num2) return num1;
-    else if (num1 > num2) return num2 + SumNaturalNumbers(num1, num2++);
-    else if (num1 < num2) return num2 + SumNaturalNumbers(num1, num2--);
+    if (num1 > num2) return num2 + SumNaturalNumbers(num1, num2+1);
+    else if (num1 < num2) return num2 + SumNaturalNumbers(num1, num2-1);
+    else return num1;
 }
-
-    /*
-    else (num1 < num2)
-    {
-        //num2+SumNaturalNumbers(num1, num2-1, 0);
-        sum = sum + (num2--);
-        Console.Write($"Сумма равно: {sum}");
-        return;
-    }*/
-    
-    //sum = sum + (num1++);
-    //SumNaturalNumbers(num1, num2, sum);
-
-
-//Console.Write($"{sum}");
